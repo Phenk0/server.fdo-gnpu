7be66ed65e04cf41cfc94262327ae48aefa9ba77
Roman Parkhomenko <roman.s.parkhomenko@gmail.com>
2024-08-22 21:27:54 +0300

    Implemented global error handling & writed custom Error class

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 // importing ROUTES
 const newsRouter = require("./routes/newsRoutes");
 const staffRouter = require("./routes/staffRoutes");
@@ -15,6 +17,7 @@ if (process.env.NODE_ENV === "development") {
 
 //MIDDLEWARE: modify requests (POST, PUT, PATCH, DELETE) to get json from body
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 //MIDDLEWARE: add request time to all requests
 app.use((req, res, next) => {
@@ -26,5 +29,11 @@ app.use((req, res, next) => {
 app.use("/api/v1/news", newsRouter);
 app.use("/api/v1/staff", staffRouter);
 app.use("/api/v1/users", usersRouter);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
