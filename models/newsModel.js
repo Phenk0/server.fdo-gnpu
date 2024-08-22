1b22d87d2d4c1489330e731532690ce487c2edb6
Roman Parkhomenko <roman.s.parkhomenko@gmail.com>
2024-08-22 06:12:00 +0300

    improved newsModel & stuffModel: add validadion

--- models/newsModel.js
+++ models/newsModel.js
@@ -5,25 +5,51 @@ const newsSchema = new Schema({
   tags: {
     type: [String],
     required: true,
-    enum: [
-      "NEWS",
-      "EVENT",
-      "GUEST_LECTURE",
-      "PRACTICE",
-      "EXTRACURRICULAR",
-      "STUDENT_SCIENCE",
-      "VACANCIES",
-      "PRIDE",
-      "TEST",
-    ],
+    enum: {
+      values: [
+        "NEWS",
+        "EVENT",
+        "GUEST_LECTURE",
+        "PRACTICE",
+        "EXTRACURRICULAR",
+        "STUDENT_SCIENCE",
+        "VACANCIES",
+        "PRIDE",
+        "TEST",
+      ],
+      message: "Виберіть дійсну категорію",
+    },
     default: ["NEWS"],
   },
-  title: { type: String /*, required: [true, "Новина має містити назву"] */ },
+  title: { type: String },
   shortCut: { type: String, trim: true },
-  imagesList: { type: [String] },
+  imagesList: {
+    type: [String],
+    validate: [
+      function (urlsArr) {
+        // If the array is empty, null, or undefined, pass the validation
+        if (!urlsArr || urlsArr.length === 0) {
+          return true;
+        }
+        // Regex to validate URLs
+        const urlRegex =
+          /^(https?:\/\/)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(:\d+)?(\/\S*)?$/;
+        // Check that every URL in the array matches the regex
+        return urlsArr.every((imageUrl) => urlRegex.test(imageUrl));
+      },
+      "Будь-ласка вкажіть дійсну адресу зберігання зображень", // Custom error message
+    ],
+  },
   text: { type: [String] },
   component: { type: String },
-  pdfUrl: { type: String, trim: true },
+  pdfUrl: {
+    type: String,
+    trim: true,
+    match: [
+      /^(https?:\/\/)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(:\d+)?(\/\S*)?$/,
+      "Будь-ласка вкажіть дійсну адресу зберігання PDF документа",
+    ],
+  },
   createdAt: {
     type: Date,
     required: [true, "Новина має містити вірну дату"],
@@ -34,7 +60,13 @@ const newsSchema = new Schema({
   },
   linkTo: { type: String, trim: true },
   // For FUTURE:
-  ratingAverage: { type: Number, default: 0, select: false },
+  ratingAverage: {
+    type: Number,
+    default: 4.5,
+    select: false,
+    min: [1, "Рейтинг має бути не менше 1"],
+    max: [5, "Рейтинг має бути не більше 5"],
+  },
   ratingQuantity: { type: Number, default: 0, select: false },
   hidden: { type: Boolean, default: false },
 });
@@ -42,6 +74,11 @@ const newsSchema = new Schema({
 // QUERY MIDDLEWARE
 newsSchema.pre(/^find/, function (next) {
   this.find().where("hidden").ne(true).select("-hidden");
+  next();
+});
+// removes -hidden- from aggregate query
+newsSchema.pre("aggregate", function (next) {
+  this.pipeline().unshift({ $match: { hidden: { $ne: true } } });
   next();
 });
 
