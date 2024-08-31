abafc36100abfe95c9e75bd81b574d0cbf32385e
Roman Parkhomenko <roman.s.parkhomenko@gmail.com>
2024-08-31 21:50:43 +0300

    security improvements: limited number of requests per hour for IP; add helmet middleware

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -10,20 +12,37 @@ const usersRouter = require("./routes/usersRoutes");
 
 const app = express();
 
-//MIDDLEWARE: log all requests
+//1) GLOBAL MIDDLEWARES:
+// Set security HTTP headers
+app.use(helmet());
+
+// Log all requests in development
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-//MIDDLEWARE: modify requests (POST, PUT, PATCH, DELETE) to get json from body
-app.use(express.json());
+// Limit requests from same IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 1000 * 60 * 15,
+  message: "Забагато запитів з цього IP. Спробуйте пізніше через 15 хвилин",
+});
+app.use("/api", limiter);
+
+//Body parser: modify requests (POST, PUT, PATCH, DELETE) to get json from body
+app.use(
+  express.json({
+    limit: "10kb",
+  }),
+);
+
+// Serve static files
 app.use(express.static(`${__dirname}/public`));
 
-//MIDDLEWARE: add request time to all requests
+// Add request time to all requests
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
 
-  console.log("app.js:26", req.headers);
   next();
 });
 
